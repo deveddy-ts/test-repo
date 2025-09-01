d2d5a0745e0d0f4257d717a905d4f55303b5c5c4
Edet Asuquo <eddy1759@gmail.com>
2025-09-01 16:32:12 +0100

    refactor: remove TODO and FIXME comments from app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-// TODO: This is a test comment that should be flagged by Review Guardian.
-
 function main() {
-    // FIXME: This is a fixme comment that should be flagged by Review Guardian.
   return "hello world";
 }
