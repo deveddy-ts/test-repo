5eadb10aad2b74c5f7ba8ec877daab091fa00949
Edet Asuquo <eddy1759@gmail.com>
2025-09-01 16:03:04 +0100

    fix: add fixme comment to be flagged by Review Guardian

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // TODO: This is a test comment that should be flagged by Review Guardian.
 
 function main() {
+    // FIXME: This is a fixme comment that should be flagged by Review Guardian.
   return "hello world";
 }
