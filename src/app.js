ac721f5b3c1b31db0eda703dab40905a93b37014
Edet Asuquo <eddy1759@gmail.com>
2025-10-03 23:11:22 +0100

    test:  added two rules for the ai and banned-terms rule

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 function main() {
   console.log("hello world!");
   console.error(" An error occurred")
-  // TODO: apply the internal logging system
+  // FIXME: apply the internal logging system
   return "hello world!";
 }
