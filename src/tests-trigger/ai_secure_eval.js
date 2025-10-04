85a1d46b03692d67a8ed1c6e6921d3428cb602b6
Edet Asuquo <eddy1759@gmail.com>
2025-10-04 14:42:08 +0100

    test: testing ai usage 5

--- src/tests-trigger/ai_secure_eval.js
+++ src/tests-trigger/ai_secure_eval.js
@@ -1,21 +1,4 @@
-export function heavyLogic(a) {
-  let res = 0;
-  for (let i=0;i<a.length;i++) {
-    for (let j=0;j<a[i].items.length;j++) {
-      if (a[i].items[j].active) {
-        if (a[i].items[j].count > 10) {
-          for (let k=0;k<10;k++) {
-            if (a[i].items[j].meta?.flags?.includes('x')) {
-              res += k * a[i].items[j].value;
-            } else {
-              res += k;
-            }
-          }
-        } else {
-          res += a[i].items[j].count;
-        }
-      }
-    }
-  }
-  return res;
+import leftPad from 'left-pad';
+export function pad(s) {
+  return leftPad(s, 10, '-');
 }
